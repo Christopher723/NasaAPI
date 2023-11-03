389794e23eab69121b48ee50b73cc117cbb98d1a
Christopher <Christopher.woods723@gmail.com>
2023-11-02 20:36:18 -0400

    Initial Commit

---
+++ NasaAPI/NasaAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NasaAPIApp.swift
+//  NasaAPI
+//
+//  Created by Christopher Woods on 11/2/23.
+//
+
+import SwiftUI
+
+@main
+struct NasaAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
